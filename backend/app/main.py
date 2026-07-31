92ff491f98230016afb5f4a3a2873d1cf743b880
SarveshBankar <118747123+SarveshBankar@users.noreply.github.com>
2026-07-31 19:30:46 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,9 +35,13 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://spendsense-frontend-jcd0.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
